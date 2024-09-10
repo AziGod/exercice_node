f25edcb85483819a2d98a1137ac065901e130721
AziGod <armandlecourt2003@gmail.com>
2024-09-10 15:21:51 +0200

    Ajout du lien hypertexte pour télécharger.

--- index.js
+++ index.js
@@ -22,12 +22,36 @@ app.get('/readfile', (req, res) => {
             res.status(500).send('Erreur lors de la lecture du fichier', err);
         } else {
             // Envoyez le contenu du fichier en réponse (à compléter)
-            res.send('Contenu du fichier: ', data); // Complétez ici pour envoyer le contenu du fichier via res
+            res.send(data); // Complétez ici pour envoyer le contenu du fichier via res
         }
     });
 });
+
+
+
+
+
+
+// Route qui affiche la page html pour donner un lien qui télécharge le fichier data
+const path = require('path');
+
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'telecharger.html'));
+});
+
+app.get('/telecharger', (req, res) => {
+    const file = path.join(__dirname, 'data.txt'); // Chemin vers le fichier
+    res.download(file, 'data.txt', (err) => {
+        if (err) {
+            console.error('Erreur lors du téléchargement du fichier:', err);
+            res.status(500).send('Erreur lors du téléchargement.');
+        }
+    });
+});
+
 
 // Démarrez le serveur pour qu'il écoute sur le port défini (à compléter)
 app.listen(PORT, () => { // Complétez avec le port sur lequel votre serveur va écouter
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
+
